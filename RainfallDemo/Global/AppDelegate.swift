ffd16c1386e19c3703fde3f65c42dd3ebcadbe0b
Mounika <Mounika@Mounikas-MacBook-Air.local>
2017-11-14 10:49:27 +0530

    Login screen UI

--- RainfallDemo/Global/AppDelegate.swift
+++ RainfallDemo/Global/AppDelegate.swift
@@ -16,6 +16,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        let initialViewController = LoginVC(nibName: "LoginView", bundle: nil) as LoginVC
+        let navigationController = UINavigationController()
+        navigationController.viewControllers = [initialViewController]
+        navigationController.navigationBar.isHidden = true
+        self.window?.rootViewController = navigationController
+        self.window?.makeKeyAndVisible()
         return true
     }
 
